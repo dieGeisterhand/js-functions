026cdc32b8637908746b98f9ed14485231089553
jeyfrem <jeyfrem@proton.me>
2024-04-12 11:50:20 -0400

    useful stuff

---
+++ main.js
@@ -0,0 +1,4 @@
+// use getElem(id) to target DOM elements by their ID. 
+// use by assigning it to a variable with the element ID as function parameter -> const var = getElem('id');
+
+const getElem = (id) => document.getElementById(id);
